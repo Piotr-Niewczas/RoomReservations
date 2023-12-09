20384c7d1aa8e65d0dc7df543f2b776917b85304
Piotr Niewczas <piotr.niewczas2002@gmail.com>
2023-12-09 16:11:57 +0100

    Added unit tests to test adding Reservations, rooms, and transactions to the in memory db

---
+++ RoomReservations.Tests/Models/RRDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+
+namespace RoomReservations.Tests.Models
+{
+    internal class RRDbContext(DbContextOptions<RRDbContext> options) : DbContext(options)
+    {
+        public DbSet<RoomReservations.Models.Room> Rooms { get; set; }
+        public DbSet<RoomReservations.Models.Reservation> Reservations { get; set; }
+        public DbSet<RoomReservations.Models.Transaction> Transactions { get; set; }
+        public static DbContextOptions<RRDbContext> DefaultOptions = new DbContextOptionsBuilder<RRDbContext>()
+                .UseInMemoryDatabase("RRTests")
+                .Options;
+    }
+}
