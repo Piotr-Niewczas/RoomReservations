bc4ef03718d1d090c2f27b900317f88c76f0f541
Piotr Niewczas <piotr.niewczas2002@gmail.com>
2023-12-09 21:13:59 +0100

    added IRoomService to get room data from db added MockData class to contain and add mock data to db updated ShowRooms to use IRoomService

---
+++ RoomReservations/Data/RoomService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoomReservations.Models;
+
+namespace RoomReservations.Data
+{
+    public interface IRoomService
+    {
+        Task<List<Room>> GetRoomsAsync();
+    }
+
+    public class RoomService : IRoomService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RoomService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Room>> GetRoomsAsync()
+        {
+            return await _context.Rooms.ToListAsync();
+        }
+    }
+
+}
