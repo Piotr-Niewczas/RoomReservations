6e1963c81b026f1700aaa354506ba955954320bf
Piotr-Niewczas <piotr.niewczas2002@gmail.com>
2023-12-11 15:41:28 +0100

    added DeleteAllReservations()

--- RoomReservations/Data/ReservationService.cs
+++ RoomReservations/Data/ReservationService.cs
@@ -3,39 +3,44 @@ using RoomReservations.Models;
 
 namespace RoomReservations.Data
 {
-	public interface IReservationService
-	{
-		Task<Reservation> AddReservationAsync(Reservation reservation);
-		Task<List<Reservation>> GetReservationsAsync();
-		Task<List<Reservation>> GetReservationsBetweenAsync(DateTime startDate, DateTime endDate);
-	}
+    public interface IReservationService
+    {
+        Task<Reservation> AddReservationAsync(Reservation reservation);
+        Task<List<Reservation>> GetReservationsAsync();
+        Task<List<Reservation>> GetReservationsBetweenAsync(DateTime startDate, DateTime endDate);
+    }
 
-	public class ReservationService : IReservationService
-	{
-		private readonly ApplicationDbContext _context;
+    public class ReservationService : IReservationService
+    {
+        private readonly ApplicationDbContext _context;
 
-		public ReservationService(ApplicationDbContext context)
-		{
-			_context = context;
-		}
+        public ReservationService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-		public async Task<List<Reservation>> GetReservationsAsync()
-		{
-			return await _context.Reservations.ToListAsync();
-		}
+        public async Task<List<Reservation>> GetReservationsAsync()
+        {
+            return await _context.Reservations.ToListAsync();
+        }
 
-		public async Task<List<Reservation>> GetReservationsBetweenAsync(DateTime startDate, DateTime endDate)
-		{
-			return await _context.Reservations
-				.Where(r => !(startDate >= r.EndDate || endDate <= r.StartDate))
-				.ToListAsync();
-		}
+        public async Task<List<Reservation>> GetReservationsBetweenAsync(DateTime startDate, DateTime endDate)
+        {
+            return await _context.Reservations
+                .Where(r => !(startDate >= r.EndDate || endDate <= r.StartDate))
+                .ToListAsync();
+        }
 
-		public async Task<Reservation> AddReservationAsync(Reservation reservation)
-		{
-			_context.Reservations.Add(reservation);
-			await _context.SaveChangesAsync();
-			return reservation;
-		}
-	}
+        public async Task<Reservation> AddReservationAsync(Reservation reservation)
+        {
+            _context.Reservations.Add(reservation);
+            await _context.SaveChangesAsync();
+            return reservation;
+        }
+        public async Task DeleteAllReservations()
+        {
+            _context.Reservations.RemoveRange(_context.Reservations);
+            await _context.SaveChangesAsync();
+        }
+    }
 }
