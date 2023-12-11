5f06694ea2608db18fcd55bf46c7afeb7ebf0414
Piotr-Niewczas <piotr.niewczas2002@gmail.com>
2023-12-11 15:32:49 +0100

    added services for Reservations and one for getting Available (not reserved) rooms in date range

---
+++ RoomReservations/Data/ReservationService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoomReservations.Models;
+
+namespace RoomReservations.Data
+{
+	public interface IReservationService
+	{
+		Task<Reservation> AddReservationAsync(Reservation reservation);
+		Task<List<Reservation>> GetReservationsAsync();
+		Task<List<Reservation>> GetReservationsBetweenAsync(DateTime startDate, DateTime endDate);
+	}
+
+	public class ReservationService : IReservationService
+	{
+		private readonly ApplicationDbContext _context;
+
+		public ReservationService(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<List<Reservation>> GetReservationsAsync()
+		{
+			return await _context.Reservations.ToListAsync();
+		}
+
+		public async Task<List<Reservation>> GetReservationsBetweenAsync(DateTime startDate, DateTime endDate)
+		{
+			return await _context.Reservations
+				.Where(r => !(startDate >= r.EndDate || endDate <= r.StartDate))
+				.ToListAsync();
+		}
+
+		public async Task<Reservation> AddReservationAsync(Reservation reservation)
+		{
+			_context.Reservations.Add(reservation);
+			await _context.SaveChangesAsync();
+			return reservation;
+		}
+	}
+}
