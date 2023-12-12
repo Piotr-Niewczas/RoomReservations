bf27461c1ec5b6ac9dfc7415399224ef513990e3
Piotr-Niewczas <piotr.niewczas2002@gmail.com>
2023-12-12 17:51:38 +0100

    added some sample data and a basic broken test

--- RoomReservations.Tests/Data/ReservationServiceTests.cs
+++ RoomReservations.Tests/Data/ReservationServiceTests.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RoomReservations.Data;
+using RoomReservations.Models;
+using RoomReservations.Tests.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +13,66 @@ namespace RoomReservations.Data.Tests
     [TestClass()]
     public class ReservationServiceTests
     {
-        [TestMethod()]
-        public void GetReservationsAsyncTest()
-        {
+        private ApplicationDbContext _context = null!;
+        private IReservationService _reservationService = null!;
 
+        readonly List<Room> rooms =
+           [
+               new Room
+               {
+                   Name = "Test Room 1",
+                   PricePerNight = 30.50M
+               },
+               new Room
+               {
+                   Name = "Test Room 2",
+                   PricePerNight = 130.99M
+               },
+               new Room
+               {
+                   Name = "Test Room 3",
+                   PricePerNight = 99.99M
+               }
+           ];
+        readonly List<Reservation> reservations = [];
+        private readonly DateTime _date = new(2000, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _context = TestsContextOptions.TestingContext;
+            _reservationService = new ReservationService(_context);
+        }
+
+        [TestMethod()]
+        public async Task GetReservationsAsyncTest()
+        {
+            reservations.Add(new Reservation
+            {
+                StartDate = _date.AddDays(1),
+                EndDate = _date.AddDays(2),
+                Rooms =
+                [
+                    rooms[0]
+                ],
+                Transactions = []
+            });
+            reservations.Add(new Reservation
+            {
+                StartDate = _date.AddDays(3),
+                EndDate = _date.AddDays(5),
+                Rooms =
+                [
+                    rooms[1]
+                ],
+                Transactions = []
+            });
+
+            _context.Reservations.AddRange(reservations);
+            List<Reservation> result = await _reservationService.GetReservationsAsync();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Count, reservations.Count);
         }
 
         [TestMethod()]
@@ -45,6 +103,12 @@ namespace RoomReservations.Data.Tests
         public void IsRoomReservedInDateRangeTest()
         {
 
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _context.Dispose();
         }
     }
 }
