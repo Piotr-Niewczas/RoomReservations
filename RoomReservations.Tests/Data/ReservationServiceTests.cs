958f741a2e02974ddd0841ee170e75824995ed0f
Piotr-Niewczas <piotr.niewczas2002@gmail.com>
2023-12-12 14:13:08 +0100

    made methods public cuz tests mad added empty tests

---
+++ RoomReservations.Tests/Data/ReservationServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoomReservations.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomReservations.Data.Tests
+{
+    [TestClass()]
+    public class ReservationServiceTests
+    {
+        [TestMethod()]
+        public void GetReservationsAsyncTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetReservationsBetweenAsyncTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void AddReservationAsyncTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void DeleteAllReservationsTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void AreAnyRoomsReservedInDateRangeTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void IsRoomReservedInDateRangeTest()
+        {
+
+        }
+    }
+}
