70adb0fc1ed4d29fc107db73dcd0d1ec51eddcd1
Piotr-Niewczas <piotr.niewczas2002@gmail.com>
2023-12-19 20:19:36 +0100

    added required links to DbContext

--- RoomReservations/Data/ApplicationDbContext.cs
+++ RoomReservations/Data/ApplicationDbContext.cs
@@ -9,5 +9,25 @@ namespace RoomReservations.Data
         public DbSet<Reservation> Reservations { get; set; }
         public DbSet<Room> Rooms { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
+
+        public DbSet<RoomReservation> RoomReservations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<RoomReservation>()
+                .HasKey(rr => new { rr.RoomId, rr.ReservationId });
+
+            modelBuilder.Entity<RoomReservation>()
+                .HasOne(rr => rr.Room)
+                .WithMany(r => r.RoomReservations)
+                .HasForeignKey(rr => rr.RoomId);
+
+            modelBuilder.Entity<RoomReservation>()
+                .HasOne(rr => rr.Reservation)
+                .WithMany(re => re.RoomReservations)
+                .HasForeignKey(rr => rr.ReservationId);
+        }
     }
 }
