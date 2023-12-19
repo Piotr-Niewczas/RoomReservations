dc679393bf4db7dd9a32ab032dcca6c18e99fd6d
Piotr-Niewczas <piotr.niewczas2002@gmail.com>
2023-12-19 21:41:35 +0100

    made changes in models to add ReservationTransactions

--- RoomReservations/Models/Reservation.cs
+++ RoomReservations/Models/Reservation.cs
@@ -1,12 +1,12 @@
 ﻿namespace RoomReservations.Models
 {
-    public class Reservation
-    {
-        public int Id { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public bool IsPaid { get; set; } = false;
-        public List<RoomReservation> RoomReservations { get; set; } = [];
-        public List<Transaction> Transactions { get; set; } = [];
-    }
+	public class Reservation
+	{
+		public int Id { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public bool IsPaid { get; set; } = false;
+		public List<RoomReservation> RoomReservations { get; set; } = [];
+		public List<ReservationTransaction> ReservationTransactions { get; set; } = [];
+	}
 }
---
+++ RoomReservations/Models/ReservationTransaction.cs
@@ -0,0 +1,11 @@
+﻿namespace RoomReservations.Models
+{
+	public class ReservationTransaction
+	{
+		public int ReservationId { get; set; }
+		public Reservation Reservation { get; set; } = null!;
+
+		public int TransactionId { get; set; }
+		public Transaction Transaction { get; set; } = null!;
+	}
+}
