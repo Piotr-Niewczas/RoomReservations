e3503f928225488ad6ae540e0689edb97a6b1c5c
Piotr-Niewczas <piotr.niewczas2002@gmail.com>
2023-12-19 21:15:23 +0100

    added necessary migration

---
+++ RoomReservations/Data/Migrations/20231219201332_AddRoomReservations.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoomReservations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoomReservations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rooms_Reservations_ReservationId",
+                table: "Rooms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rooms_ReservationId",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "ReservationId",
+                table: "Rooms");
+
+            migrationBuilder.CreateTable(
+                name: "RoomReservations",
+                columns: table => new
+                {
+                    RoomId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ReservationId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomReservations", x => new { x.RoomId, x.ReservationId });
+                    table.ForeignKey(
+                        name: "FK_RoomReservations_Reservations_ReservationId",
+                        column: x => x.ReservationId,
+                        principalTable: "Reservations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoomReservations_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomReservations_ReservationId",
+                table: "RoomReservations",
+                column: "ReservationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoomReservations");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ReservationId",
+                table: "Rooms",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_ReservationId",
+                table: "Rooms",
+                column: "ReservationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rooms_Reservations_ReservationId",
+                table: "Rooms",
+                column: "ReservationId",
+                principalTable: "Reservations",
+                principalColumn: "Id");
+        }
+    }
+}
