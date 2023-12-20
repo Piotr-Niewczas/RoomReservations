a641af04ce5c544e37dad70564f1c23d2e57b13b
Piotr-Niewczas <piotr.niewczas2002@gmail.com>
2023-12-20 23:11:05 +0100

    remade and re-enabled the WithRooms_ReturnsMatchingReservations test

--- RoomReservations.Tests/Data/ReservationsServiceTests/SearchReservationsTests.cs
+++ RoomReservations.Tests/Data/ReservationsServiceTests/SearchReservationsTests.cs
@@ -127,70 +127,86 @@ namespace RoomReservations.Tests.Data.ReservationsServiceTests
             Assert.AreEqual(result.Count, 1);
         }
 
-        //[TestMethod] // Currencly not working due to missconfiguration of the database
-        //public void SearchReservations_WithRooms_ReturnsMatchingReservations()
-        //{
-        //    // Arrange
-        //    List<Room> rooms =
-        //       [
-        //           new Room
-        //           {
-        //               Name = "Test Room 1",
-        //               PricePerNight = 30.50M
-        //           },
-        //           new Room
-        //           {
-        //               Name = "Test Room 2",
-        //               PricePerNight = 130.99M
-        //           }
-        //       ];
-        //    _context.Rooms.AddRange(rooms);
-        //    _context.SaveChanges();
+        [TestMethod]
+        public void SearchReservations_WithRooms_ReturnsMatchingReservations()
+        {
+            // Arrange
+            List<Room> rooms =
+               [
+                   new Room
+                   {
+                       Name = "Test Room 1",
+                       PricePerNight = 30.50M
+                   },
+                   new Room
+                   {
+                       Name = "Test Room 2",
+                       PricePerNight = 130.99M
+                   }
+               ];
+            _context.Rooms.AddRange(rooms);
+            _context.SaveChanges();
 
-        //    var roomsFromDb = _context.Rooms.ToList();
-        //    var roomsToFind = new List<Room>
-        //    {
-        //        roomsFromDb[0]
-        //    };
-        //    _context.Reservations.Add(new Reservation
-        //    {
-        //        StartDate = DateTime.Now.AddDays(-2),
-        //        EndDate = DateTime.Now,
-        //        Rooms = new List<Room> { _context.Rooms.First(r => r.Name == "Test Room 1") }
-        //    });
-        //    _context.SaveChanges();
+            var date = DateTime.Now;
+            List<Reservation> reservations = [
+                new Reservation
+                {
+                    StartDate = date.AddDays(-2),
+                    EndDate = date,
+                    RoomReservations =
+                    [
+                        new RoomReservation
+                        {
+                            Room = _context.Rooms.First(r => r.Name == rooms[0].Name)
+                        }
+                    ]
+                },
+                new Reservation
+                {
+                    StartDate = date.AddDays(1),
+                    EndDate = date.AddDays(4),
+                    RoomReservations =
+                    [
+                        new RoomReservation
+                        {
+                            Room = _context.Rooms.First(r => r.Name == rooms[0].Name)
+                        },
+                        new RoomReservation
+                        {
+                            Room = _context.Rooms.First(r => r.Name == rooms[1].Name)
+                        }
+                    ]
+                },
+                new Reservation
+                {
+                    StartDate = date.AddDays(4),
+                    EndDate = date.AddDays(10),
+                    RoomReservations =
+                    [
+                        new RoomReservation
+                        {
+                            Room = _context.Rooms.First(r => r.Name == rooms[1].Name)
+                        }
+                    ]
+                }
+            ];
 
-        //    _context.Reservations.Add(new Reservation
-        //    {
-        //        StartDate = DateTime.Now.AddDays(1),
-        //        EndDate = DateTime.Now.AddDays(4),
-        //        Rooms = new List<Room> { _context.Rooms.First(r => r.Name == "Test Room 1"), _context.Rooms.First(r => r.Name == "Test Room 2") }
-        //    });
-        //    _context.SaveChanges();
+            _context.Reservations.AddRange(reservations); ;
+            _context.SaveChanges();
+            var roomsFromDb = _context.Rooms.ToList();
+            var roomsToFind = new List<Room>
+            {
+                roomsFromDb[0]
+            };
 
-        //    _context.Reservations.Add(new Reservation
-        //    {
-        //        StartDate = DateTime.Now.AddDays(4),
-        //        EndDate = DateTime.Now.AddDays(10),
-        //        Rooms = new List<Room> { _context.Rooms.First(r => r.Name == "Test Room 2") }
-        //    });
-        //    _context.SaveChanges();
+            // Act
+            var result = _reservationService.SearchReservations(rooms: roomsToFind);
 
-        //    var reservationsInDb = _context.Reservations.ToList();
-
-        //    Assert.AreEqual(3, reservationsInDb[0].Rooms.Count);
-
-        //    List<Room> roomsToFind = [rooms[0]];
-
-        //    // Act
-        //    var result = _reservationService.SearchReservations(rooms: roomsToFind);
-
-        //    // Assert
-        //    Assert.IsNotNull(result);
-        //    Assert.IsTrue(result.All(r => r.Rooms.Any(room => roomsToFind.Contains(room))));
-        //    Assert.AreEqual(result.Count, 2);
-        //    Assert.AreEqual(result[0].Rooms[0].Name, rooms[0].Name);
-
-        //}
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.All(r => r.RoomReservations.Any(rr => roomsToFind.Contains(rr.Room))));
+            Assert.AreEqual(result.Count, 2);
+            Assert.AreEqual(result.First().RoomReservations.First().Room.Name, rooms.First().Name);
+        }
     }
 }
