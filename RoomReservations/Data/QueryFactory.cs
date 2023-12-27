be36a2d521ef8ff9a8030a3b8b49bfdfe56dd18b
Piotr-Niewczas <piotr.niewczas2002@gmail.com>
2023-12-27 17:31:23 +0100

    add QueryFactory

---
+++ RoomReservations/Data/QueryFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace RoomReservations.Data;
+
+public class QueryFactory(ApplicationDbContext context)
+{
+    public IQueryable<T> Create<T>() where T : class
+    {
+        return context.Set<T>().AsQueryable();
+    }
+}
