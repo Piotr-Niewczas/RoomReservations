2685ccafd0cc9a9413ae0f15f58d651e0c6aab49
Piotr-Niewczas <piotr.niewczas2002@gmail.com>
2023-12-28 15:35:07 +0100

    Models cleanup

--- RoomReservations/Models/Room.cs
+++ RoomReservations/Models/Room.cs
@@ -1,20 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace RoomReservations.Models
-{
-    public class Room
-    {
-        public int Id { get; set; }
-        [Required(AllowEmptyStrings = false)]
-        public string Name { get; set; } = String.Empty;
-        public string Description { get; set; } = String.Empty;
-        public int Capacity { get; set; }
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal PricePerNight { get; set; }
-        public string Location { get; set; } = String.Empty;
-        public string ImageUrl { get; set; } = "img/rooms/notfound.jpg";
+namespace RoomReservations.Models;
 
-        public List<RoomReservation> RoomReservations { get; set; } = [];
-    }
+public class Room
+{
+    public int Id { get; set; }
+    [Required(AllowEmptyStrings = false)] public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int Capacity { get; set; }
+    [Column(TypeName = "decimal(18,2)")] public decimal PricePerNight { get; set; }
+    public string Location { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = "img/rooms/notfound.jpg";
+
+    public List<RoomReservation> RoomReservations { get; set; } = [];
 }
