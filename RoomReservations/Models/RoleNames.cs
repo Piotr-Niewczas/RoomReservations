fb2561bb8a5436ccf43e33e8bafe1a5d5dab751d
Piotr-Niewczas <piotr.niewczas2002@gmail.com>
2023-12-29 17:54:16 +0100

    add RoleNames.cs

---
+++ RoomReservations/Models/RoleNames.cs
@@ -0,0 +1,12 @@
+﻿namespace RoomReservations.Models;
+
+public static class RoleNames
+{
+    public static readonly List<string> Roles =
+    [
+        "Admin",
+        "Receptionist",
+        "Employee",
+        "Client"
+    ];
+}
