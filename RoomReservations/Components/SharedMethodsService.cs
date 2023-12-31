93281fbbd88d2160e303ca7ec4887930452e814b
Piotr-Niewczas <piotr.niewczas2002@gmail.com>
2023-12-31 17:05:57 +0100

    add SharedMethodsService.cs and add it to Program.cs

---
+++ RoomReservations/Components/SharedMethodsService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.JSInterop;
+using RoomReservations.Models;
+
+namespace RoomReservations.Components;
+
+public enum GetCurrentUserError
+{
+    Success,
+    UserNotLoggedIn,
+    HttpContextIsNull
+}
+
+public class SharedMethodsService(
+    UserManager<ApplicationUser> userManager,
+    IHttpContextAccessor httpContextAccessor,
+    IJSRuntime jsRuntime)
+{
+    private ApplicationUser? _loggedInUser;
+
+    public async void GoBackAsync()
+    {
+        // Navigate to previous page user was on using JS
+        await jsRuntime.InvokeVoidAsync("history.back");
+    }
+
+    public async Task<(ApplicationUser?, GetCurrentUserError)> GetCurrentUserAsync()
+    {
+        // If we already have the user, return it
+        if (_loggedInUser != null) return (_loggedInUser, GetCurrentUserError.Success);
+
+        if (httpContextAccessor.HttpContext is null) return (null, GetCurrentUserError.HttpContextIsNull);
+
+        var user = await userManager.GetUserAsync(httpContextAccessor.HttpContext.User);
+        if (user is null) return (null, GetCurrentUserError.UserNotLoggedIn);
+
+        _loggedInUser = user;
+        return (_loggedInUser, GetCurrentUserError.Success);
+    }
+}
