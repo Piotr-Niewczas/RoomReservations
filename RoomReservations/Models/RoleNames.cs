f559247ed06721c1dc0559cfb46e33c3f867922b
Piotr-Niewczas <piotr.niewczas2002@gmail.com>
2024-01-01 22:11:01 +0100

    replaced hard coded role names with constants from RoleNames.cs

--- RoomReservations/Models/RoleNames.cs
+++ RoomReservations/Models/RoleNames.cs
@@ -2,11 +2,16 @@
 
 public static class RoleNames
 {
+    public const string Admin = "Admin";
+    public const string Receptionist = "Receptionist";
+    public const string Employee = "Employee";
+    public const string Client = "Client";
+
     public static readonly List<string> Roles =
     [
-        "Admin",
-        "Receptionist",
-        "Employee",
-        "Client"
+        Admin,
+        Receptionist,
+        Employee,
+        Client
     ];
 }
