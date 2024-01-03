cbf2b83fbb4d7686d75847aa39d7a4ad5efb90ce
Piotr-Niewczas <piotr.niewczas2002@gmail.com>
2024-01-03 21:02:35 +0100

    added UserService.cs

---
+++ RoomReservations/Data/UserService.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Identity;
+using RoomReservations.Models;
+
+namespace RoomReservations.Data;
+
+public class UserService(UserManager<ApplicationUser> userManager)
+{
+    public async Task<ApplicationUser> AddUserAsync(string email, string firstName, string lastName)
+    {
+        var user = new ApplicationUser
+        {
+            UserName = email,
+            Email = email,
+            FirstName = firstName,
+            LastName = lastName
+        };
+
+        var result = await userManager.CreateAsync(user, GeneratePassword());
+        if (result.Succeeded)
+            await userManager.AddToRoleAsync(user, RoleNames.Client);
+        else
+            throw new Exception($"Failed to create user {email}. Error: {result.Errors}");
+
+        return user;
+    }
+
+    public async Task<ApplicationUser?> FindUserByEmailAsync(string email)
+    {
+        return await userManager.FindByEmailAsync(email);
+    }
+
+    private static string GeneratePassword(int baseLength = 16)
+    {
+        var random = new Random();
+        var password = new StringBuilder();
+        for (var i = 0; i < baseLength; i++)
+        {
+            var randomChar = (char)random.Next(97, 122); // a-z
+            password.Append(randomChar);
+        }
+
+        var howManyUpperCase = random.Next(1, 4);
+        for (var i = 0; i < howManyUpperCase; i++)
+        {
+            var randomChar = (char)random.Next(65, 90); // A-Z
+            var randomIndex = random.Next(0, password.Length);
+            password.Insert(randomIndex, randomChar);
+        }
+
+        var howManyNumbers = random.Next(1, 4);
+        for (var i = 0; i < howManyNumbers; i++)
+        {
+            var randomChar = (char)random.Next(48, 57); // 0-9
+            var randomIndex = random.Next(0, password.Length);
+            password.Insert(randomIndex, randomChar);
+        }
+
+        var howManySpecialChars = random.Next(1, 4);
+        for (var i = 0; i < howManySpecialChars; i++)
+        {
+            var randomChar = (char)random.Next(33, 47); // !-/
+            var randomIndex = random.Next(0, password.Length);
+            password.Insert(randomIndex, randomChar);
+        }
+
+        return password.ToString();
+    }
+}
