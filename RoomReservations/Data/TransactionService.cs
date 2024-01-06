febe992f5904367bec1542de87bf2275852d11d9
Piotr-Niewczas <piotr.niewczas2002@gmail.com>
2024-01-06 17:29:03 +0100

    created TransactionService.cs and added it to services

---
+++ RoomReservations/Data/TransactionService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoomReservations.Models;
+
+namespace RoomReservations.Data;
+
+public interface ITransactionService
+{
+    /// <summary>
+    ///     Add transaction to the database
+    /// </summary>
+    /// <param name="reservation"> Reservation to add transaction to </param>
+    /// <param name="amount"> Amount </param>
+    /// <returns> ID of added transaction </returns>
+    Task<int> AddTransactionAsync(Reservation reservation, decimal amount);
+
+    Task PayTransactionAsync(int transactionId);
+    IQueryable<Transaction> CreateTransactionQuery();
+}
+
+public class TransactionService(ApplicationDbContext context) : ITransactionService
+{
+    public async Task<int> AddTransactionAsync(Reservation reservation, decimal amount)
+    {
+        var reservationTransaction = new ReservationTransaction
+        {
+            Reservation = reservation,
+            Transaction = new Transaction
+            {
+                Amount = amount,
+                EntryDate = DateTime.Now
+            }
+        };
+        reservation.ReservationTransactions.Add(reservationTransaction);
+        await context.SaveChangesAsync();
+        return reservationTransaction.Transaction.Id;
+    }
+
+    public async Task PayTransactionAsync(int transactionId)
+    {
+        var reservationTransaction = context.ReservationTransactions
+            .Include(rt => rt.Transaction)
+            .First(rt => rt.Transaction.Id == transactionId);
+        reservationTransaction.Transaction.AccountingDate = DateTime.Now;
+        await context.SaveChangesAsync();
+    }
+
+    public IQueryable<Transaction> CreateTransactionQuery()
+    {
+        return new QueryFactory(context).Create<Transaction>();
+    }
+}
